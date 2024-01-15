1b043d9eb12f221f723b73b4dd9b442ec7bd39f6
Sinan Engin <snn.engn@yandex.com>
2024-01-15 23:35:32 +0300

    image hover eklendi

--- 4.hafta/script.js
+++ 4.hafta/script.js
@@ -1,13 +1,8 @@
 var rastgele= document.getElementById("rastgele");
-
-
-
 for ( let i = 0; i<9; i++) {
 
-    rastgele.innerHTML += `<img src="https://source.unsplash.com/random/${i + 1}" class="shadow-lg rounded mx-3 my-3" width="350" height="300" alt="photo">`;
-
+    rastgele.innerHTML += `<img id="img" src="https://source.unsplash.com/random/${i + 1}" class="shadow-lg rounded mx-3 my-3" width="350" height="300" alt="photo">`;
     }
-
 
 var yenile= document.getElementById("yenile")
 
