523e889eef9cbaa57071c88f3f64227f6ff05050
Sinan Engin <snn.engn@yandex.com>
2024-02-26 00:59:56 +0300

    vercel uyumluluk

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,19 +1,10 @@
-import axios from "axios";
 import Navbar from "./components/Navbar";
-import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import SiteRoutes from "./SiteRoutes";
 import data from "/src/data/data.json"
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios("/src/data/data.json")
-      .then((res) => {
-        setData(res.data);
-      })
-      .catch((e) => console.log(e));
-  }, []);
+
 
   return (
     <>
