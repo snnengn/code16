c05d4154aac1dda34c3aa588b930fbaa06763c55
Sinan Engin <108197925+snnengn@users.noreply.github.com>
2024-03-29 12:36:34 +0300

    Update Footer.jsx

--- final-project/src/components/Footer.jsx
+++ final-project/src/components/Footer.jsx
@@ -3,7 +3,7 @@
 function Footer() {
   return (
     <>
-    <footer className='bg-light border-top mt-3 position-sticky bottom-0'>
+    <footer className='bg-light border-top mt-3 position-sticky'>
 <div className="container">
   <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-2">
     <div className="col-md-4 d-flex align-items-center">
