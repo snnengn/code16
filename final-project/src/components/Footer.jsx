44597ec9db480c570b51686091fa1828c750b923
Sinan Engin <108197925+snnengn@users.noreply.github.com>
2024-03-29 13:03:37 +0300

    Update Footer.jsx

--- final-project/src/components/Footer.jsx
+++ final-project/src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
   <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-2">
     <div className="col-md-4 d-flex align-items-center">
       <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-      <img src="https://www.bursa.bel.tr/assets/images/logo_dark1.png" style={{height:75}} alt="" />
+      <img src="https://www.bursa.bel.tr/assets/images/logo_dark1.png" style={{height:50}} alt="" />
       </a>
           </div>
 
